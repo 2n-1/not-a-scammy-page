f5deec0988058ea9aa3fd74920dd01edd8b15a37
stefano amorelli <stefano@amorelli.tech>
2023-09-19 17:40:32 +0300

    Add .gitignore, .vscode/extensions.json, README.md, index.html, package.json, public/vite.svg, src/App.vue, src/assets/vue.svg, src/components/HelloWorld.vue, src/main.ts, src/style.css, src/vite-env.d.ts, tsconfig.json, tsconfig.node.json, vite.config.ts. These changes were made to initialize the project, add necessary configuration files, and create the basic structure and content of the application.

---
+++ src/components/HelloWorld.vue
@@ -0,0 +1,67 @@
+<script setup lang="ts"></script>
+
+<template>
+  <img
+    src="https://www.shutterstock.com/shutterstock/photos/2305897469/display_1500/stock-vector-you-win-golden-text-with-fire-flying-sparks-game-banner-vector-illustration-2305897469.jpg"
+    width="350"
+  />
+  <h3 class="big"><strike>Stefano's New Gucci Shoes Fund</strike></h3>
+  <h3>Tallinn BSides Lottery</h3>
+  <img
+    class="today"
+    src="https://media2.giphy.com/media/TgyqiV0vOoHdgAPVU5/giphy.gif?cid=6c09b952ivpgl4pmc8dzo5gq94cdft9w9w0o0eop8i4ti1jg&ep=v1_internal_gif_by_id&rid=giphy.gif&ct=ts"
+  />
+  <h2>You hve just won $ 1,932,522.19</h2>
+  <p>ENTER YOUR CREDIT CARD DETAILS, & NAME OF YOUR FIRST PET HERE:</p>
+  <form>
+    <input placeholder="your credit card" />
+    <input placeholder="name of your first pet" />
+    <input class="button" type="submit" value="Redeem NOW!" />
+  </form>
+  <img
+    src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcSy38wuv8oLl8YOEk8szGXhcbCXzrSRwCy0jA&usqp=CAU"
+  />
+</template>
+
+<style scoped>
+h1 {
+  font-size: 2em;
+  color: red;
+}
+h2 {
+  font-size: 1.8m;
+}
+p {
+  font-size: 1.5em;
+}
+form {
+  padding: 20px;
+}
+form input {
+  margin: 0 10px;
+}
+h3 {
+  font-size: 1.5em;
+}
+input {
+  font-size: 20px;
+}
+.today {
+  width: 200px;
+}
+p {
+  color: green;
+}
+.button {
+  background-color: purple;
+  color: white;
+}
+* {
+  font-family: "Times new roman";
+}
+h3 {
+  font-weight: bold;
+  font-size: 1.2em;
+  text-transform: uppercase;
+}
+</style>
